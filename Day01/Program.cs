c4024b5208939cc96c04a6b54ef86bda09ba1611
Marco Moinho <marco.moinho@gmail.com>
2024-12-01 08:36:04 +0100

    Add project files.

---
+++ Day01/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] inputs = File.ReadAllLines("input.txt");
+
+List<int> left = [];
+List<int> right = [];
+
+foreach (string line in inputs)
+{
+    string[] tmp = line.Split("   ");
+    left.Add(int.Parse(tmp[0]));
+    right.Add(int.Parse(tmp[1]));
+}
+
+left.Sort();
+right.Sort();
+
+int part1 = 0;
+for (int i = 0; i < left.Count; i++)
+    part1 += Math.Abs(left[i] - right[i]);
+
+Console.WriteLine($"Part 1: {part1}");
+
+int part2 = 0;
+left.ForEach(n => part2 += n * right.Count(x => x == n));
+
+Console.WriteLine($"Part 2: {part2}");
